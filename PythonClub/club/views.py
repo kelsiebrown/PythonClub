8cb9af635b482f153d6a9fe9d03ac4a41144c5a8
Kelsie Brown <38116231+kelsiebrown@users.noreply.github.com>
2019-02-06 11:26:43 -0800

    Add assignment 6 meeting and meeting view

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from .models import Resource, Meeting
 
 # Create your views here.
 def index(request):
@@ -8,3 +8,12 @@ def index(request):
 def resources (request):
     type_list=Resource.objects.all()
     return render (request, 'club/resources.html', {'type_list': type_list})
+
+def meetings (request):
+    meetings_list = Meeting.objects.all()
+    return render (request, 'club/meetings.html', {'meetings_list': meetings_list})
+
+def meetingdetail (request, id):
+    detail = get_object_or_404(Meeting, pk=id)
+    context = {'detail': detail}
+    return render (request, 'club/details.html', context=context)
